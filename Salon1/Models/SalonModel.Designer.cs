d4386075abae713dcabcd013e4eca165fddd9c66
gerprog - Gerard Daly <ger_daly@yahoo.com>
2014-04-20 20:12:57 +0100

    pulling and merge team explorer test

--- Salon1/Models/SalonModel.Designer.cs
+++ Salon1/Models/SalonModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\j\Documents\salon\Salon1\Models\SalonModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Ger\Source\Repos\salon\Salon1\Models\SalonModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
