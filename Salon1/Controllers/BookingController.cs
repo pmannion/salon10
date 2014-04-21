a6225fae3aee827d4794b85fc1e6d0c041108df6
gerprog - Gerard Daly <ger_daly@yahoo.com>
2014-04-21 17:29:53 +0100

    customers made at time of booking, drop down needs bit of work

--- Salon1/Controllers/BookingController.cs
+++ Salon1/Controllers/BookingController.cs
@@ -50,20 +50,29 @@ namespace Salon1.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include="BookingID,TimeStart,EndTime,CustomerID,StaffID,StatusID")] Booking booking)
+
+
+        public ActionResult Create([Bind(Include = "CustomerID,LastName,FirstName,Gender,DOB,BookingID,TimeStart,EndTime,StaffID,StatusID")] Customer customer, Booking booking)
         {
             if (ModelState.IsValid)
             {
+                db.Customers.Add(customer);
+                db.SaveChanges();
                 db.Bookings.Add(booking);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-
+            //return View(customer);
+        {
             ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "LastName", booking.CustomerID);
             ViewBag.StaffID = new SelectList(db.Staffs, "StaffID", "FirstName", booking.StaffID);
             ViewBag.StatusID = new SelectList(db.Status, "StatusID", "Description", booking.StatusID);
             return View(booking);
         }
+    }
+
+
+
 
         // GET: /Booking/Edit/5
         public ActionResult Edit(int? id)
@@ -88,7 +97,7 @@ namespace Salon1.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include="BookingID,TimeStart,EndTime,CustomerID,StaffID,StatusID")] Booking booking)
+        public ActionResult Edit([Bind(Include = "BookingID,TimeStart,EndTime,CustomerID,StaffID,StatusID")] Booking booking)
         {
             if (ModelState.IsValid)
             {
